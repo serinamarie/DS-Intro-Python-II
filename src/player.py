6b45eece638b7c02db5b6fd8b63e0c8fe5a0d981
Serina Grill <gril0044@umn.edu>
2020-07-16 22:08:11 -0400

    changed format of adv.py into player.py

--- src/player.py
+++ src/player.py
@@ -2,7 +2,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, direction=None):
         self.name = name
         self.current_room = current_room
 
@@ -13,16 +13,16 @@ class Player:
             "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
         if direction == 'n':
-            if not self.current_room.n_to:
+            if self.current_room.n_to == None:
                 error_message = ("-------------------------------------------------------------",
                 "Wrong way, cowboy. You can't go north here.",
                 "Try a new direction!",
                 "-------------------------------------------------------------\n")
                 print('\n'.join(error_message))
-        else:
-            self.current_room = self.current_room.n_to
+            else:
+                self.current_room = self.current_room.n_to
         
-        if direction == 's':
+        elif direction == 's':
             if not self.current_room.s_to:
                 error_message = ("-------------------------------------------------------------",
                 "Uh oh. There's no way to go south here so keep your pants on.",
@@ -33,7 +33,7 @@ class Player:
                 self.current_room = self.current_room.s_to
                 print('\n'.join(congratulatory_message))
         
-        if direction == 'e':
+        elif direction == 'e':
             if not self.current_room.e_to:
                 error_message = ("-------------------------------------------------------------", 
                 "Going east? You've just walked right into a wall.",
@@ -44,8 +44,8 @@ class Player:
                 self.current_room = self.current_room.e_to
                 print('\n'.join(congratulatory_message))
         
-        if direction == 'w':
-            if not self.current_room.w_to:
+        elif direction == 'w':
+            if self.current_room.w_to == None:
                 error_message = ("-------------------------------------------------------------",
                 "Going west? You're the best. Just not at this game.",
                 "Try a new direction!",
@@ -54,8 +54,11 @@ class Player:
             else:
                 self.current_room = self.current_room.w_to
                 print('\n'.join(congratulatory_message))
+        elif direction == 'q':
+            "You gave up fast!"
+            sys.exit()
 
 if __name__ == "__main__": 
-    player_1 = Player(current_room='outside')
-    print(player_1)
+    player_1 = Player(name='Serina', current_room='outside')
+    print(player_1.move('n'))
  
